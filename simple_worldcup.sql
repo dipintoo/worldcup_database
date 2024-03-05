a885c0d33e3dae1a277e59c464b0ec9b026fb1f9
dipintoo <120252968+dipintoo@users.noreply.github.com>
2024-03-05 17:48:51 +0700

    Update simple_worldcup.sql

--- simple_worldcup.sql
+++ simple_worldcup.sql
@@ -1,16 +1,16 @@
--- Membuat tabel teams
+-- Tabel Teams
 CREATE TABLE teams (
     team_id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL
 );
 
--- Membuat tabel games
+-- Tabel Games
 CREATE TABLE games (
     game_id SERIAL PRIMARY KEY,
     year INTEGER NOT NULL,
     round VARCHAR(255) NOT NULL,
-    winner_id INTEGER REFERENCES teams(teams_id),
-    opponent_id INTEGER REFERENCES teams(teams_id),
     winner_goals INTEGER NOT NULL,
     opponent_goals INTEGER NOT NULL
+    winner_id INTEGER REFERENCES teams(teams_id),
+    opponent_id INTEGER REFERENCES teams(teams_id),
 );
