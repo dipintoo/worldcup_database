3b31a1b88f9bc58de5c572fd9c280bf152371f61
dipintoo <120252968+dipintoo@users.noreply.github.com>
2024-03-06 19:23:01 +0700

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -9,7 +9,7 @@ fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
-# Fungsi untuk memeriksa dan memasukkan tim ke dalam tabel jika belum ada
+# Fungsi untuk memeriksa dan menginput tim jika belum ada
 insert_team() {  
   TEAM_NAME=$($PSQL "SELECT name FROM teams WHERE name = '$1'")
   if [[ -z $TEAM_NAME ]]
@@ -19,23 +19,22 @@ insert_team() {
   fi
 }
 
-# Membaca file games.csv
-tail -n +2 games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+# Membaca file games.csv lalu menetapkan kolom / variabel inputnya
+tail -n +2 games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS; do
 
-do
-  # Jika tidak ada, Input team ke tabel teams
+  # Jika tidak ada, Input nama tim ke tabel teams
   insert_team "$WINNER"
   insert_team "$OPPONENT"
 
-  # Jika ada, ambil ID team dari tabel teams
+  # Jika ada, ambil ID tim agar terhubung ke tabel games
   WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER'")
   OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT'")
 
-  # Input team ke tabel games
+  # Input detail pertandingan ke tabel games
   $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) \
            VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)"
    )
 
-  # Cetak games ke terminal
+  # Cetak pesan berhasil menambahkan data
   echo New game added: $WINNER_ID VS $OPPONENT_ID , score $WINNER_GOALS : $OPPONENT_GOALS
 done
